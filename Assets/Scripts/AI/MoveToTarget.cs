f330830988eb81a0ce301a0ca813580ad2012b9a
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-09-02 12:05:46 +0200

    Start AI

---
+++ Assets/Scripts/AI/MoveToTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour
+{
+    [SerializeField]
+    private string tag;
+    [SerializeField]
+    private float speed = 1;
+    private GameObject target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag(tag);
+    }
+
+    void Update()
+    {
+        GoToTarget(target);
+    }
+
+    private void GoToTarget(GameObject targ)
+    {
+        float step = speed * Time.deltaTime;
+        Vector3 pos = new Vector2(targ.transform.position.x, targ.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, pos, step);
+    }
+}
