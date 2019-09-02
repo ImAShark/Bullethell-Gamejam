1f09916041b871b216422d0a1c245cccf1632456
BradleyBekker <thekid72gaming@gmail.com>
2019-09-02 12:43:04 +0200

    added a scene for testing  added movement script  bagan shooting

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed = 2;
+    void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime );
+
+    }
+}
